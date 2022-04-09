6986a213d3270e149bd1405c978494415cae2415
keeezy <lazarokev@gmail.com>
2022-04-09 02:12:18 -0700

    Created a function to prompt for selection of character length/type for password

--- script.js
+++ script.js
@@ -60,12 +60,82 @@ var upperCaseChar = [
   'Z'
 
 ];
+
+var numericalChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+var specialChar = [ 
+  '!',
+  '#',
+  '$',
+  '%',
+  '&',
+  "'",
+  '(',
+  ')',
+  '*',
+  '+',
+  ',',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+  '[',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~',
+  '"'
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+function generatePassword() {
+  var optionsSelected = getUserSelection()
+  if (optionsSelected === undefined) {
+    return;
+  }
+}
+
+
+// Series of prompts for user selection
+function getUserSelection() {
+  var numberSelection = parseInt(prompt("Enter number of characters for password, must be between 8 and 128 characters "))
+  if (Number.isNaN(numberSelection)) {
+    alert("You must enter a number")
+  }else if (numberSelection < 8) {
+    alert("Minimum 8 characters needed.")
+  }else if (numberSelection > 128) {
+    alert("Cannot exceed more than 128")
+  }
+
+var useLowerCase = confirm("Include lower case characters?")
+var useUpperCase = confirm("Include upper case characters?")
+var useNumerical = confirm("Include numbers?")
+var useSpecial = confirm("Include special characters?")
+
+if(!useLowerCase && !useUpperCase && !useNumerical && !useSpecial) {
+  alert("You need to select at least of one character type")
+}
+
+}
+
+
+
 
 }
 
