3acb35c80986b70c0ea59e9d45f4432a1f8a2e98
keeezy <lazarokev@gmail.com>
2022-04-09 01:15:42 -0700

    Decalred the global vars

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declared Variables
+var lowerCaseChar = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var upperCaseChar = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+
+];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
